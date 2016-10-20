fb6cffb6c60bfbc8eb24abb1c8b10dc62b0bc4ae
René Winkler <renewinkler@gmx.ch>
2016-10-20 13:51:46 +0200

    setup project

---
+++ js/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+//var $ = require('jquery');
+//window.$ = $;
+//require('bootstrap');
+
+// short babel test
+var values = [3,2,1];
+var result = values.sort((a,b) => a-b);
+console.log(result);
