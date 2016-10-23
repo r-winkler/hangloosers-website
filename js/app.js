806af99ef362270f065da7590bbf1bc207e1586d
René Winkler <renewinkler@gmx.ch>
2016-10-23 11:55:30 +0200

    design contact-us section

--- js/app.js
+++ js/app.js
@@ -56,3 +56,12 @@ $(function() {
 		);
 	});
 });
+
+// Facebook like us plugin
+(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/de_DE/sdk.js#xfbml=1&version=v2.8&appId=1595978190662701";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
